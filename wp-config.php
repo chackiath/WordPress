ea07928b91fc7a7d258842188aee8c71f4c50ca0
chackiath <61818070+chackiath@users.noreply.github.com>
2020-08-12 15:20:15 +0530

    update mu properties

--- wp-config.php
+++ wp-config.php
@@ -78,7 +78,14 @@ $table_prefix = 'wp_';
  * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
 define( 'WP_DEBUG', false );
-
+define( 'WP_ALLOW_MULTISITE', true );
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', false);
+define('DOMAIN_CURRENT_SITE', 'test1.dnsrobo.com');
+define('PATH_CURRENT_SITE', '/');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
+define('SUNRISE', true);
 /* That's all, stop editing! Happy publishing. */
 
 /** Absolute path to the WordPress directory. */
